fe45bddf75fbb42f9b0484f3032051ebb8beaee2
Richard Vincent <rich@richardvincent.com>
2016-03-24 21:58:11 +0000

    Blogful app with unittesting, acceptance testing, and integration testing added

--- blog/config.py
+++ blog/config.py
@@ -9,3 +9,10 @@ class DevelopmentConfig(object):
         5432/blogful".format(psqlUser, psqlPassword)
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "")
+
+
+class TestingConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@localhost:\
+       5432/blogful-test".format(psqlUser, psqlPassword)
+    DEBUG = False
+    SECRET_KEY = "Not secret"
