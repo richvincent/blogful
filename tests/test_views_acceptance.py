6c25de1f529068bd501d679550a5a22ea01eee69
Richard Vincent <rich@richardvincent.com>
2016-03-28 23:21:49 +0000

    Postgres Role issue fixed let's see any new issues

--- tests/test_views_acceptance.py
+++ tests/test_views_acceptance.py
@@ -7,8 +7,9 @@ from urllib.parse import urlparse
 from werkzeug.security import generate_password_hash
 from splinter import Browser
 
-# Configure your app to use the testing database
-os.environ["CONFIG_PATH"] = "blog.config.TestingConfig"
+# Configure your app to use the testing configuration
+if not "CONFIG_PATH" in os.environ:
+    os.environ["CONFIG_PATH"] = "blog.config.TestingConfig"
 
 from blog import app
 from blog.database import Base, engine, session, User
